aeaf17178701109e885d90fe97ade5470cd63e66
X2Proad <153321056+chamesayari@users.noreply.github.com>
2024-01-24 20:00:50 +0100

    Update js tag

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,6 @@ function makeTest(emailToTest) {
 
         fetch('https://20.84.48.24:5000/sender/testemail', {
             method: 'POST',
-			mode: "cors",
             body: formData
             //body: formData, agent
         })
